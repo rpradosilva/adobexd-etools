40aa226f90b3f9a659d7036e8d4ed232d0c583cf
rpradosilva <contato@rprado.design>
2021-10-27 01:09:55 -0300

    refactor: update copy action to enable filter

--- new/commands/copy.js
+++ new/commands/copy.js
@@ -1,6 +1,5 @@
 async function copyEmails(filter) {
   const emailsList = await selectEmails(filter);
-  console.log(emailsList);
   await createEmailPlaceholder(emailsList);
   copyToClipboard();
   removeElement("#emails-placeholder", "[data-auto='inviteContainer']");
