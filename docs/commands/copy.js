1d15b4a2149e8c836ce7fe179a3ab487e6b882c9
Rafael Prado <contato@rprado.design>
2021-11-15 15:48:18 -0300

    refactor: remove old files

--- docs/commands/copy.js
+++
@@ -1,6 +0,0 @@
-async function copyEmails(filter) {
-  const emailsList = await selectEmails(filter);
-  await createEmailPlaceholder(emailsList);
-  copyToClipboard();
-  removeElement("#emails-placeholder", "[data-auto='inviteContainer']");
-}
