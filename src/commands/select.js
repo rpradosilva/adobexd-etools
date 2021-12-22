317e7b7f3d1f1460f97c27ebe5ba6e8210f46e61
Rafael Prado <contato@rprado.design>
2021-12-22 11:44:08 -0300

    refactor: remove all console log

--- src/commands/select.js
+++ src/commands/select.js
@@ -4,11 +4,8 @@ function selectEmails(filter) {
     "[data-auto='inviteContainer'] ul li .ccx-ss-user-card-details [aria-label]"
   );
 
-  console.log(filter);
-
   for (const user of usersList) {
     let email = user.getAttribute("aria-label").toLowerCase();
-    console.log(email);
     if (
       (email.indexOf("@") > 0 && email.indexOf(filter) >= 0) ||
       (email.indexOf("@") > 0 && filter === undefined) ||
@@ -16,7 +13,6 @@ function selectEmails(filter) {
     ) {
       emailsList.push(sanitizeEmails(email));
     }
-    console.log(emailsList);
   }
 
   function sanitizeEmails(email) {
